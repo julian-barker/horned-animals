4ef476ebd95e4c7ac3f9f178c414ed71dd088bd7
Julian Barker <julian.c.barker@gmail.com>
2022-09-19 15:22:40 -0700

    complete lab01

--- src/App.js
+++ src/App.js
@@ -1,31 +1,48 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import Header from './Header.js';
 import Main from './Main.js';
 import Footer from './Footer.js';
+import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Header />
-      <Main />
-      <Footer />
-    </div>
-  );
+const beasts = [{
+  "_id": 1,
+  "image_url": "http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg",
+  "title": "UniWhal",
+  "description": "A unicorn and a narwhal nuzzling their horns",
+  "keyword": "narwhal",
+  "horns": 1
+},
+
+{
+  "_id": 2,
+  "image_url": "https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80",
+  "title": "Rhino Family",
+  "description": "Parent rhino with two babies",
+  "keyword": "rhino",
+  "horns": 2
+},
+
+{
+  "_id": 3,
+  "image_url": "https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg",
+  "title": "Unicorn Head",
+  "description": "Someone wearing a very silly unicorn head mask",
+  "keyword": "unicorn",
+  "horns": 1
+}];
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Main beasts={beasts} />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Main.js
+++ src/Main.js
@@ -1,23 +1,38 @@
-class HornedBeast {
-  constructor (title, imageUrl, description) {
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
+import React from "react";
+
+class HornedBeast extends React.Component {
+  render() {
+    return(
+      <div>
+        <h2>{this.props.title}</h2>
+        <img src={this.props.src} alt={this.props.alt} />
+        <p>{this.props.description}</p>
+      </div>
+    )
   }
-
-
 } 
 
-function Main(beasts) {
-  return (
-    <main>
-      {
-        beasts.forEach( el => {
-          
-        })
-      }
-    </main>
-  );
+
+class Main extends React.Component {
+  render() {
+    return (
+      <main>
+        {
+          this.props.beasts.map( el => {
+            return (
+              <HornedBeast 
+                src={el.image_url}
+                alt={el.title}
+                description={el.description}
+                key={el.keyword}
+              />
+            )
+          })
+        }
+      </main>
+    );
+  }
 }
+
 
 export default Main;
