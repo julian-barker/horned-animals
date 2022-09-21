7799c44180ad1600c3c4833a9ccd585251d97d6d
Julian Barker <julian.c.barker@gmail.com>
2022-09-20 17:41:21 -0700

    implement sort by id, name, horns, popularity

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -3,24 +3,23 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
 class HornedBeast extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {likes: 0};
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick() {
-    this.setState(state => ({ likes: state.likes + 1 }));
+  
+  handleClick = () => {
+    this.props.click(this.props.id);
   }
 
   render() {
+    const compact = `💛s: ${new Intl.NumberFormat({notation: 'compact'}).format(this.props.likes)}`;
+    console.log(compact);
+    const likes = this.props.likes === 0 ? 'No 💛s' : this.props.likes < 20 ? '💛'.repeat(this.props.likes) : compact;
     return(
       <Card style={{ width: '18rem' }} >
         <Card.Img variant="top" src={this.props.src} alt={this.props.title} />
         <Card.Body>
           <Card.Title>{this.props.title}</Card.Title>
           <Card.Text>{this.props.description}</Card.Text>
-          <Card.Text>❤'s: {'💛'.repeat(this.state.likes)}</Card.Text>
+          {/* <Card.Text>❤'s: {'💛'.repeat(this.props.likes)}</Card.Text> */}
+          <Card.Text>{likes}</Card.Text>
           <Button variant="primary" onClick={this.handleClick}>Favorite</Button>
         </Card.Body>
       </Card>
--- src/Main.js
+++ src/Main.js
@@ -1,24 +1,72 @@
 import React from "react";
 import HornedBeast from './HornedBeast.js';
+import Button from 'react-bootstrap/Button';
 
 
 class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    const temp = this.props.beasts.slice();
+    temp.forEach(beast => beast.likes = 0);
+    this.state = {beasts: temp};
+  }
+
+  favorite = (id) => {
+    const temp = this.state.beasts.slice();
+    const i = temp.findIndex(el => id === el._id);
+    temp[i].likes += 1;
+    this.setState({ beasts: temp });
+  }
+
+  sortByHorns = () => {
+    const temp = this.state.beasts.slice();
+    temp.sort((a,b) => b.horns - a.horns);
+    this.setState({ beasts: temp });
+  }
+
+  sortByLikes = () => {
+    const temp = this.state.beasts.slice();
+    temp.sort((a,b) => b.likes - a.likes);
+    this.setState({ beasts: temp });
+  }
+
+  sortByTitle = () => {
+    const temp = this.state.beasts.slice();
+    temp.sort((a,b) => a.title.localeCompare(b.title));
+    this.setState({ beasts: temp });
+  }
+  
+  sortById = () => {
+    const temp = this.state.beasts.slice();
+    temp.sort((a,b) => a._id - b._id);
+    this.setState({ beasts: temp });
+  }
+
+  renderBeast(beast) {
+    return (
+      <HornedBeast 
+        key={beast._id}
+        id={beast._id}
+        src={beast.image_url}
+        title={beast.title}
+        description={beast.description}
+        likes={beast.likes}
+        click={(id) => this.favorite(id)}
+        className="col-4"
+      />
+    )
+  }
+
   render() {
     return (
       <main className="container-fluid" >
-        <div className="row row-cols-auto justify-content-between g-4" >
+        <Button variant="primary" onClick={this.sortByHorns}>Sort by Number of Horns</Button>
+        <Button variant="primary" onClick={this.sortByLikes}>Sort by Popularity</Button>
+        <Button variant="primary" onClick={this.sortByTitle}>Sort Alphabetically</Button>
+        <Button variant="primary" onClick={this.sortById}>Reset to Default</Button>
+        <div className="row row-cols-auto justify-content-evenly g-4" >
           {
-            this.props.beasts.map( el => {
-              return (
-                <HornedBeast 
-                  src={el.image_url}
-                  title={el.title}
-                  description={el.description}
-                  key={el._id}
-                  className="col-4"
-                />
-              )
-            })
+            this.state.beasts.map( beast => this.renderBeast(beast) )
           }
         </div>
       </main>
